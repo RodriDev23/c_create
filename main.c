15f9a4d35a0a883e2ed64bc5581d60b96fa02f85
Rodri <RodriDev23@outlook.com>
2024-07-02 16:23:09 -0400

    adding things

--- main.c
+++ main.c
@@ -3,49 +3,66 @@
 #include <string.h>
 
 int create_folder(char *name_project) {
-    char space_name[150];
-    snprintf(space_name, sizeof(space_name), "mkdir %s", name_project);
-    system(space_name);
-    return 0;
+  char space_name[150];
+  snprintf(space_name, sizeof(space_name), "mkdir %s", name_project);
+  system(space_name);
+  return 0;
 }
 
-int create_file(char *name_project) {
-    char path[200];
-    snprintf(path, sizeof(path), "%s/compile_main.sh", name_project);
+int create_file_compile(char *name_project) {
+  char path[200];
+  snprintf(path, sizeof(path), "%s/compile_main.sh", name_project);
 
-    FILE *compile_file = fopen(path, "w");
-    if (compile_file == NULL) {
-        printf("Error opening file!\n");
-        return 1;
-    }
+  FILE *compile_file = fopen(path, "w");
+  if (compile_file == NULL) {
+    printf("Error opening file!\n");
+    return 1;
+  }
 
-    char *compile_text =
-        "#!/bin/bash\n"
-        "\n"
-        "gcc main.c -o main\n"
-        "\n"
-        "if [ $? -eq 0 ]; then\n"
-        "    echo \"Compile it successfully\"\n"
-        "else\n"
-        "    echo \"Compilation failed\"\n"
-        "    exit 1\n"
-        "fi\n"
-        "\n"
-        "./main\n";
+  char *compile_text = "#!/bin/bash\n"
+                       "\n"
+                       "gcc main.c -o main\n"
+                       "\n"
+                       "if [ $? -eq 0 ]; then\n"
+                       "    echo \"Compile it successfully\"\n"
+                       "else\n"
+                       "    echo \"Compilation failed\"\n"
+                       "    exit 1\n"
+                       "fi\n"
+                       "\n"
+                       "./main\n";
 
-    fprintf(compile_file, "%s", compile_text);
-    fclose(compile_file);
-    return 0;
+  fprintf(compile_file, "%s", compile_text);
+  fclose(compile_file);
+  return 0;
+}
+
+int create_file_c(char *name_project) {
+  char path[200];
+  snprintf(path, sizeof(path), "%s/main.c", name_project);
+
+  FILE *compile_file = fopen(path, "w");
+  if (compile_file == NULL) {
+    printf("Error opening file!\n");
+    return 1;
+  }
+
+  char *compile_text = "#include <stdio.h>\n"
+                       "int main(int argc, char *argv[]) { printf("
+                      "\"hello world\""
+                       ")\n; }";
+  fprintf(compile_file, "%s", compile_text);
+  fclose(compile_file);
+  return 0;
 }
 
 int main() {
-    char name_project[150];
-    printf("Enter the name of your project: ");
-    scanf("%s", name_project); // No need to limit scanf with a length
+  char name_project[150];
+  printf("Enter the name of your project: ");
+  scanf("%s", name_project); // No need to limit scanf with a length
 
-    create_folder(name_project);
-    create_file(name_project);
-
-    return 0;
+  create_folder(name_project);
+  create_file_compile(name_project);
+  create_file_c(name_project);
+  return 0;
 }
-
